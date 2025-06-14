f5396e90a3a817758547ac9923eff03a55a4c597
J0R6IT0 <jorgeparpar@gmail.com>
2025-06-14 20:41:00 +0200

    chore: manifest fields

--- src/primitives/elements.rs
+++ src/primitives/elements.rs
@@ -1,5 +1,6 @@
 use gpui::*;
 
+/// Shorthand for creating a `Div` element with a text child.
 pub fn span(text: impl Into<SharedString>) -> Div {
     div().child(text.into())
 }
--- src/primitives/layout.rs
+++ src/primitives/layout.rs
@@ -1,9 +1,11 @@
 use gpui::*;
 
+/// Shorthand for creating a vertical flex `Div` element.
 pub fn v_flex() -> Div {
     div().flex().flex_col()
 }
 
+/// Shorthand for creating a horizontal flex `Div` element.
 pub fn h_flex() -> Div {
     div().flex().flex_row()
 }
